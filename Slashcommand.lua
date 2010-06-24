def052929b00f007d01bd47ee7b552a34ed4adbf
Shane Sveller <shanesveller@gmail.com>
2010-06-24 16:16:23 -0400

    Chatframe messages for slash commands

--- Slashcommand.lua
+++ Slashcommand.lua
@@ -9,11 +9,17 @@ SlashCmdList[myname:upper()] = function(msg)
   	ns:CheckFriendsMatches()
   elseif msg == "clear"
     ns.db.friends = {}
+    ns:Print("Data on your Real ID friends' alts has been cleared.")
   elseif msg == "undo"
     ns:UndoLastRemoval()
   elseif msg == "undoall"
     ns:UndoAllRemovals()
   elseif msg == "auto"
     ns.db.auto = not ns.db.auto
+    if ns.db.auto then
+      ns:Print("Now automatically scanning your friends list on login.")
+    else
+      ns:Print("No longer automatically scanning your friends list on login.")
+    end
   end
 end
