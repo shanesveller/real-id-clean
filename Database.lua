ba142974a86bac9e4a033c64d60c473442206bf4
Shane Sveller <shanesveller@gmail.com>
2010-06-24 20:12:22 -0400

    DB defaults behaved rather oddly with my code :(

--- Database.lua
+++ Database.lua
@@ -3,22 +3,32 @@ local myname, ns = ...
 
 
 ns.defaults = {
+  --[[
   friends = {}, -- Real ID friends
   ignored = {},
   auto = false,
+  ]]
 }
 ns.defaultsPC = {
+  --[[
   removed = {},
   recentlyRemoved = {},
+  ]]
 } -- No need to save non-Real ID friends list in DBPC (yet)
 
 
 function ns.InitDB()
+	-- local myname = "RealIDClean"
 	_G[myname.."DB"] = setmetatable(_G[myname.."DB"] or {}, {__index = ns.defaults})
 	ns.db = _G[myname.."DB"]
-
+	if not ns.db.friends then ns.db.friends = {} end
+	if not ns.db.ignored then ns.db.ignored = {} end
+	if ns.db.auto == nil then ns.db.auto = false end
+	
 	_G[myname.."DBPC"] = setmetatable(_G[myname.."DBPC"] or {}, {__index = ns.defaultsPC})
 	ns.dbpc = _G[myname.."DBPC"]
+	if not ns.dbpc.removed then ns.dbpc.removed = {} end
+	if not ns.dbpc.recentlyRemoved then ns.dbpc.recentlyRemoved = {} end
 end
 
 
