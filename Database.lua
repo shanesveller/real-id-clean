14bfd6daa9971e6eda22c90baa3fd6fba05c098c
Shane Sveller <shanesveller@gmail.com>
2010-06-28 20:02:10 -0400

    Account for old DB versions

--- Database.lua
+++ Database.lua
@@ -21,10 +21,33 @@ function ns.InitDB()
   if not ns.dbpc.removed then ns.dbpc.removed = {} end
   if not ns.dbpc.recentlyRemoved then ns.dbpc.recentlyRemoved = {} end
   if not ns.dbpc.automaticallyAdded then ns.dbpc.automaticallyAdded = {} end
+
+  if not ns.db.version or ns.db.version < 1 then ns.ImportOldData() end
 end
 
 
 function ns.FlushDB()
   for i,v in pairs(ns.defaults) do if ns.db[i] == v then ns.db[i] = nil end end
   for i,v in pairs(ns.defaultsPC) do if ns.dbpc[i] == v then ns.dbpc[i] = nil end end
+end
+
+
+function ns.ImportOldData()
+  if not ns.db.version or ns.db.version < 1 then
+    for friend,alts in pairs(RealIDCleanDB.friends) do
+      for k,v in pairs(alts) do
+        if type(v) == "string" then
+          local realm, name = strsplit("-", v)
+          ns.Debug("Convertigng " .. friend .. " - " .. realm .. ":" .. name)
+          if not ns.db.friends[friend][realm] then ns.db.friends[friend][realm] = {} end
+          if not tContains(ns.db.friends[friend][realm], name) then tinsert(ns.db.friends[friend][realm], name) end
+          ns.db.friends[friend][k] = nil
+        end
+        if k == "" then
+          ns.db.friends[friend][k] = nil
+        end
+      end
+    end
+    ns.db.version = 1
+  end
 end
